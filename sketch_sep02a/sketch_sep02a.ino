7940998187f520f1c32dcb9804c6daab0a4b5698
Stephen Qiao <stephenqiao123@gmail.com>
2022-09-21 17:40:57 -0700

    Implemented temperature and humidity
    Coded the arduino so it can read the humidity and temperature in the room and display it on the led matrix

--- sketch_sep02a/sketch_sep02a.ino
+++ sketch_sep02a/sketch_sep02a.ino
@@ -1,13 +1,20 @@
 #include "LedControl.h"
 #include "DS3231.h"
 #include "Wire.h"
+#include "DHT.h"
+
+#define Type DHT11
+int sensePin = 4;
+DHT HT(sensePin, Type);
+int humidity;
+int tempC;
 
 const int numDevices = 4;
 DS3231 myRTC;
 
 LedControl lc = LedControl(11, 13, 10, numDevices);
 
-unsigned long delaytime = 1000;
+unsigned long delayTime = 5000;
 
 int hour;
 int mins;
@@ -25,6 +32,8 @@ void setup() {
     lc.clearDisplay(i);
   }
 
+  HT.begin();
+
   myRTC.setClockMode(false);
   myRTC.setSecond(59);
   myRTC.setMinute(3);
@@ -38,6 +47,74 @@ void setup() {
   hPM = false;
 }
 
+void displayHumidityAndTemp() {
+  Serial.print("The Humidity is: ");
+  Serial.print(humidity);
+  Serial.println("The temperature is: ");
+  Serial.print(tempC);
+  
+  humidity = HT.readHumidity();
+  tempC = HT.readTemperature();
+
+  // numbers
+  byte zero[3] = {B00111110, B00100010, B00111110};
+  byte one[3] = {B00000000, B00000000, B00111110};
+  byte two[3] = {B00101110, B00101010, B00111010};
+  byte three[3] = {B00101010, B00101010, B00111110};
+  byte four[3] = {B00111000, B00001000, B00111110};
+  byte five[3] = {B00111010, B00101010, B00101110};
+  byte six[3] = {B00111110, B00101010, B00101110};
+  byte seven[3] = {B00100000, B00100000, B00111110};
+  byte eight[3] = {B00111110, B00101010, B00111110};
+  byte nine[3] = {B00111000, B00101000, B00111110};
+
+  byte* nums[10] = {zero, one, two, three, four, five, six, seven, eight, nine};
+
+  // letter C
+  byte c[3] = {B00011110,B00010010,B00010010};
+
+  // display humidity
+  // 2nd digit
+  for (int i=0;i<9;i++) {
+    if ((humidity / 10) == i) {
+      lc.setColumn(3,1, nums[i][0]);
+      lc.setColumn(3,2, nums[i][1]);
+      lc.setColumn(3,3, nums[i][2]);
+      }
+    }
+
+  // 1st digit
+  for (int i=0;i<9;i++) {
+    if ((humidity % 10) == i) {
+      lc.setColumn(3,5, nums[i][0]);
+      lc.setColumn(3,6, nums[i][1]);
+      lc.setColumn(3,7, nums[i][2]);
+      }
+    }
+
+  // display temperature
+  // 2nd digit
+  for (int i=0;i<9;i++) {
+    if ((tempC / 10) == i) {
+      lc.setColumn(2,5, nums[i][0]);
+      lc.setColumn(2,6, nums[i][1]);
+      lc.setColumn(2,7, nums[i][2]);
+      }
+    }
+
+  // 1st digit
+  for (int i=0;i<9;i++) {
+    if ((tempC % 10) == i) {
+      lc.setColumn(1,1, nums[i][0]);
+      lc.setColumn(1,2, nums[i][1]);
+      lc.setColumn(1,3, nums[i][2]);
+      }
+    }
+
+   lc.setColumn(1,5, c[0]);
+   lc.setColumn(1,6, c[1]);
+   lc.setColumn(1,7, c[2]);
+  }
 
 void writeTimeOnMatrix() {
   hour = myRTC.getHour(h12, hPM);
@@ -106,5 +183,13 @@ void writeTimeOnMatrix() {
 void loop() {
   int devices = lc.getDeviceCount();
   writeTimeOnMatrix();
-  Serial.println(myRTC.getSecond());
+  delay(delayTime);
+  for (int i=0;i<4;i++) {
+  lc.clearDisplay(i);
+  }
+  displayHumidityAndTemp();
+  delay(delayTime);
+  for (int i=0;i<4;i++) {
+  lc.clearDisplay(i);
+  }
 }
